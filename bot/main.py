5f86210075ca148745623b759d67ef4bec083f0d
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-09-04 19:11:06 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,10 +4,12 @@ import logging
 from discord.ext import commands
 import asyncpg
 import asyncio
+import sys
+import traceback
 
 if not Path("./config.py").exists():
   print("[WARNING] Configuration file not found. Please generate it by running the config generator.")
-  return
+  sys.exit(1)
 else:
   import config
 
@@ -17,9 +19,19 @@ intents.message_content = True
 
 if not config.prefix:
   print("[WARNING] Bot text prefix is missing. Please set it in config.py.")
-  return
+  sys.exit(1)
 bot = commands.Bot(command_prefix=config.prefix, intents=intents)
 bot.db_pool = asyncpg.Pool = None
 
 async def init_db():
-  if not config.db
+  if not config.database_url:
+    print("[WARNING] Database URL not found! Please set it in config.py.")
+    sys.exit(1)
+  else:
+    try:
+        bot.db_pool = await asyncpg.create_pool(dsn=config.database_url)
+        print("[INFO] Database is ready.")
+    except Exception as e:
+        print("[ERROR] Failed to connect to database.")
+        traceback.print_exception(type(e), e, e.__traceback__, file=sys.stderr)
+        sys.exit(1) 
