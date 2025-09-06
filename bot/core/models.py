2f72ead99b6c7f23750226b5887ac05812383501
AngerRandom <156929971+AngerRandom@users.noreply.github.com>
2025-09-06 16:05:25 +0300

    Update models.py

--- bot/core/models.py
+++ bot/core/models.py
@@ -2,3 +2,25 @@ from tortoise import exceptions, fields, models, signals, timezone, validators
 from enum import IntEnum
 from datetime import datetime, timedelta
 import discord
+
+class OperatingSystemType(IntEnum):
+  PROGRESSBAR = 1
+  BAROS = 2
+  PROGRESSBAR_SERVER = 3
+  HIDDEN = 4
+  EIGHT_BIT = 5
+
+class OperatingSystem(models.Model):
+  id = fields.IntField(primary_key=True)
+  name = fields.CharField(max_length=64, description="Name of the operating system")
+  type = fields.IntEnumField(
+    OperatingSystemType,
+    description="Type of the operating system",
+    default=OperatingSystemType.PROGRESSBAR
+  )
+  custom = fields.BooleanField(
+    description="If enabled, the oprerating system will be listed in custom operating systems.", 
+    default=False
+  )
+  
+  
